410444ea45347b3849fea46e3526c59dd00ec4ca
WIUT00011173 <92103077+WIUT00011173@users.noreply.github.com>
2021-12-11 20:11:00 +0500

    Add files via upload

---
+++ sorting/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxSort = new System.Windows.Forms.TextBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxSort
+            // 
+            this.tbxSort.Location = new System.Drawing.Point(376, 80);
+            this.tbxSort.Name = "tbxSort";
+            this.tbxSort.Size = new System.Drawing.Size(100, 22);
+            this.tbxSort.TabIndex = 0;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(556, 79);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.tbxSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxSort;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ sorting/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            var input = tbxSort.Text.Split(" / , ;.".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var numbers = new double[input.Length];
+            for(var i = 0; i < input.Length; i++)
+            {
+                var number = input[i];
+                if (isNumeric(number))
+                {
+                    numbers[i] = Convert.ToDouble(number);
+                    
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        swap(ref numbers[j], ref numbers[i]);
+                    }
+                }
+            }
+            MessageBox.Show(string.Join(",", numbers));
+
+            //var sort = true;
+            //while (sort)
+            //{
+            //    sort = false;
+            //    for (var i = 0; i < numbers.Length - 1; i++)
+            //    {
+            //        if (numbers[i] > numbers[i + 1])
+            //        {
+            //            sort = true;
+            //            swap(ref numbers[i], ref numbers[i + 1]);
+            //        }
+            //    }
+            //}
+            //    MessageBox.Show(string.Join(",", numbers));
+
+        }
+
+        private void swap(ref double a, ref double b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+        private bool isNumeric(string input)
+        {
+            return double.TryParse(input, out _);
+        }
+    }
+}
